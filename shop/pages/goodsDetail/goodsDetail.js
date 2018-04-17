4ca9e0ede9098e95f32c82f1d92bf7a788d1b209
unknown <dingran@uki.com>
2018-04-17 17:28:22 +0800

    4-17

--- shop/pages/goodsDetail/goodsDetail.js
+++ shop/pages/goodsDetail/goodsDetail.js
@@ -1,31 +1,149 @@
 var app = getApp()
 Page({
   data: {
-    goodsId: '',
-    goodsInfo: {},
-    modelStrs: {},
-    selectModelInfo: {
-      models: [],
-      stock: '',
-      price: '',
-      virtualPrice: '',
-      buyCount: 1,
-      models_text : ''
+    // 商品id
+    goodsId: '2',
+    goodsInfo: {
+      goods_type:0,//no
+      // 图片数组 false表示没有
+      img_urls:[
+        "https://image.octmami.com/public/images/7e/ee/65/9293f477b8edac2b6958b5ee9401184bd09bc555.jpg",
+        "https://image.octmami.com/public/images/d2/98/42/3a589c8aaddbb509addb0ec2f4c048f3e7e8085c.jpg",
+        "https://image.octmami.com/public/images/7e/ee/65/9293f477b8edac2b6958b5ee9401184bd09bc555.jpg",
+      ],
+      //默认图片
+      cover: "/images/my.png",
+      // 商品名
+      title: "费雪Fisher Price 小象安抚巾",
+      // 最低的价格
+      lowPrice: 30,
+      // 最高的价格
+      highPrice: 50,
+      // 如果最低价格等于最高价格或者最低价格=0，显示价格
+      price:60,
+      // 运费
+      express_fee:5,
+      // 库存
+      stock: 10,
     },
+    // 商品参数
+    goodsParams:[
+      {name:"重量", value:"10kg"},
+      { name: "重量", value: "10kg" },
+      { name: "重量", value: "10kg" },
+      { name: "重量", value: "10kg" },
+    ],
+    // 是否隐藏库存
+    hidestock:false,
+    // 是否隐藏商品评论
+    displayComment: false,
+    // 评论条数
+    commentNums: 10,
+    // 评论内容
+    commentExample: {
+      // 用户头像
+      buyer_headimgurl:"/images/my.png",
+      // 用户名
+      buyer_nickname:"丁冉",
+      // 购买时间
+      add_time:"2018",
+      // 评论信息
+      assess_info:{
+        // 评论内容
+        content:"好评，必须好评",
+        // 是否有图片
+        has_img:true,
+        // 图片信息
+        img_arr:[
+          "https://image.octmami.com/public/images/7e/ee/65/9293f477b8edac2b6958b5ee9401184bd09bc555.jpg",
+          "https://image.octmami.com/public/images/d2/98/42/3a589c8aaddbb509addb0ec2f4c048f3e7e8085c.jpg",
+        ]
+      }
+    },
+    // 商品详情
+    goodsDetail:{
+      // 是否是图片
+      isImg:true,
+      // 文字详情
+      detailTxt:"详情内容",
+      // 图片详情
+      detailImgs:[
+        "http://image.octmami.com/public/images/7b/c8/3b/b7052d99fdd90a87a25b1411ec80e6aa92edacc8.png?1458724526#w",
+        "http://image.octmami.com/public/images/d7/27/da/0ebf90856b3864f27e4640edf5f39138ef349251.png?1458724540#w",
+        "http://image.octmami.com/public/images/09/49/7e/1873d54fb478cb58c2fb53254ff2e8bbacdf0d8d.png?1458724572#w",
+      ]
+    },
+    // 选择商品的详情，加入购物车或者立即购买使用
+    selectModelInfo: {
+      // 图片地址
+      imgurl: "https://image.octmami.com/public/images/7e/ee/65/9293f477b8edac2b6958b5ee9401184bd09bc555.jpg",
+      title: '商品名',
+      price: '10',
+      buyCount: 1,
+      // 库存
+      stock: 10,
+      // 已选
+      models_text : '测试'
+    },
+
+
+
     pageQRCodeData:{
       shareDialogShow: "100%",
       shareMenuShow: false,
     },
-    commentNums: [],
-    commentExample: '',
-    defaultPhoto: '',
     allStock: '',
     addToShoppingCartHidden: true,
     ifAddToShoppingCart: true,
-    priceDiscountStr: '',
     page_hidden: true,
     appointmentPhone:''
   },
+  // 显示购物页面
+  showAddToShoppingCart: function () {
+    this.setData({
+      addToShoppingCartHidden: false,
+      ifAddToShoppingCart: true
+    })
+  },
+  // 隐藏购物页面
+  hiddeAddToShoppingCart: function () {
+    this.setData({
+      addToShoppingCartHidden: true
+    })
+  },
+  // 加入购物车
+  sureAddToShoppingCart: function () {
+    var that = this,
+      param = {
+        goods_id: this.data.goodsId,
+        num: this.data.selectModelInfo.buyCount,
+      };
+    // 发送添加到购物车的请求
+    // app.sendRequest({
+    //   hideLoading: true,
+    //   url: '/index.php?r=AppShop/addCart',
+    //   data: param,
+    //   success: function (res) {
+    //     app.showToast({
+    //       title: '添加成功',
+    //       icon: 'success'
+    //     });
+
+      setTimeout(function () {
+        that.hiddeAddToShoppingCart();
+      }, 1000);
+    },
+
+  
+
+
+
+
+
+
+
+
+
   onLoad: function(options){
     // options 获取打开当前页面所调用的 query 参数。
     console.log(options);
@@ -349,21 +467,8 @@ Page({
       ifAddToShoppingCart: false
     })
   },
-  showAddToShoppingCart: function(){
-    if(this.data.isSeckill && this.data.goodsInfo.seckill_start_state == 2){
-      app.showModal({content: '当前秒杀已结束，不能加入购物车'});
-      return ;
-    }
-    this.setData({
-      addToShoppingCartHidden: false,
-      ifAddToShoppingCart: true
-    })
-  },
-  hiddeAddToShoppingCart: function(){
-    this.setData({
-      addToShoppingCartHidden: true
-    })
-  },
+  
+
   selectSubModel: function(e){
     var dataset = e.target.dataset,
         modelIndex = dataset.modelIndex,
@@ -443,32 +548,7 @@ Page({
       'selectModelInfo.buyCount': count + 1
     });
   },
-  sureAddToShoppingCart: function(){
-    var that = this,
-        param = {
-                  goods_id: this.data.goodsId,
-                  model_id: this.data.selectModelInfo.modelId || '',
-                  num: this.data.selectModelInfo.buyCount,
-                  sub_shop_app_id: this.data.franchiseeId || '',
-                  is_seckill : this.data.isSeckill ? 1 : ''
-                };
 
-    app.sendRequest({
-      hideLoading: true,
-      url: '/index.php?r=AppShop/addCart',
-      data: param,
-      success: function(res){
-        app.showToast({
-          title: '添加成功',
-          icon: 'success'
-        });
-
-        setTimeout(function(){
-          that.hiddeAddToShoppingCart();
-        }, 1000);
-      }
-    })
-  },
   buyDirectlyNextStep: function(e){
     // var that = this,
     //     param = {
