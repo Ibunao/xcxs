533dd5ef2a1e5bf5996230a3bbde89afe98ac735
unknown <dingran@uki.com>
2018-05-17 17:30:13 +0800

    05-17

--- shop/pages/previewGoodsOrder/previewGoodsOrder.js
+++ shop/pages/previewGoodsOrder/previewGoodsOrder.js
@@ -28,36 +28,13 @@ Page({
     },
     // 运费
     express_fee: 5,
-    discountList: [],
-    selectDiscountInfo: {},
-    selectDiscountIndex: '',
-    
-    is_self_delivery: 0,
-    express_fee: '',
-    balance: '12',// 积分
-    useBalance: true,// 使用积分
-    deduction: '',
-    discount_cut_price: '',
+    haveIntegrals:0, // 拥有的积分
+    useIntegrals:0, // 使用的积分
+    deduction: 0,// 积分折扣金额
     // 商品价格
     original_price: '',
-    totalPayment: '',
-    shopAddress: '',
-    noAdditionalInfo: true,
-    exchangeCouponData: {
-      dialogHidden: true,
-      goodsInfo: {},
-      selectModelInfo: {},
-      hasSelectGoods: false,
-      voucher_coupon_goods_info: {}
-    }
+    totalPayment: '',// 支付金额
   },
-  isFromSelectAddress: false,
-  // 提交的商品id
-  cart_id_arr: [],
-  cart_data_arr: [],
-  requesting: false,
-  additional_info: {},
-  inputTimer: '',
   onLoad: function (options) {
     // 提交的商品信息
     this.setData({goodsList: app.globalData.goodsList})
@@ -70,6 +47,8 @@ Page({
     this.getCalculationInfo();
     // 获取地址
     this.getShopAddress();
+    // 获取积分
+    this.getIntegrals();
   },
   onShow: function(){
 
@@ -90,7 +69,33 @@ Page({
     }else{
       that.setData({ express_fee: 5 })
     }
+    // 商品金额
     that.setData({ original_price:price})
+    // 支付金额
+    that.setData({ totalPayment: price + that.data.express_fee })
+    
+  },
+  // 获取积分
+  getIntegrals:function(){
+    var that = this;
+    // 获取积分
+    wx.request({
+      url: app.globalData.host + '/xcx/get-integral',
+      // 需要更改为动态的memberid
+      data: {
+        openid: app.globalData.openid,
+        memberId: 1,
+      },
+      success: function (res) {
+        console.log(res);
+        // 存储openid
+        if (res.data.code == 200) {
+          that.setData({ haveIntegrals: res.data.other.integral })
+        } else {
+          that.setData({ haveIntegrals: 0 })
+        }
+      }
+    })
   },
   // 获取用户邮寄地址
   getShopAddress:function(){
@@ -145,109 +150,109 @@ Page({
       current: e.currentTarget.dataset.src
     });
   },
-  clickMinusButton: function(e){
-    var index = e.currentTarget.dataset.index,
-        goods = this.data.goodsList[index];
-    if(+goods.num <= 0) return;
-    this.changeGoodsNum(index, 'minus');
-  },
-  clickPlusButton: function(e){
-    var index = e.currentTarget.dataset.index;
-    this.changeGoodsNum(index, 'plus');
-  },
-  changeGoodsNum: function(index, type){
-    var goods = this.data.goodsList[index],
-        currentNum = +goods.num,
-        targetNum = type == 'plus' ? currentNum + 1 : (type == 'minus' ? currentNum - 1 : Number(type)),
-        that = this,
-        data = {},
-        param;
+  // clickMinusButton: function(e){
+  //   var index = e.currentTarget.dataset.index,
+  //       goods = this.data.goodsList[index];
+  //   if(+goods.num <= 0) return;
+  //   this.changeGoodsNum(index, 'minus');
+  // },
+  // clickPlusButton: function(e){
+  //   var index = e.currentTarget.dataset.index;
+  //   this.changeGoodsNum(index, 'plus');
+  // },
+  // changeGoodsNum: function(index, type){
+  //   var goods = this.data.goodsList[index],
+  //       currentNum = +goods.num,
+  //       targetNum = type == 'plus' ? currentNum + 1 : (type == 'minus' ? currentNum - 1 : Number(type)),
+  //       that = this,
+  //       data = {},
+  //       param;
 
-    if(targetNum == 0 && type == 'minus'){
-      app.showModal({
-        content: '确定从购物车删除该商品？',
-        showCancel: true,
-        confirm: function(){
-          that.cart_data_arr[index].num = targetNum;
-          data['goodsList['+index+'].num'] = targetNum;
-          that.setData(data);
-          that.deleteGoods(index);
-        }
-      })
-      return;
-    }
+  //   if(targetNum == 0 && type == 'minus'){
+  //     app.showModal({
+  //       content: '确定从购物车删除该商品？',
+  //       showCancel: true,
+  //       confirm: function(){
+  //         that.cart_data_arr[index].num = targetNum;
+  //         data['goodsList['+index+'].num'] = targetNum;
+  //         that.setData(data);
+  //         that.deleteGoods(index);
+  //       }
+  //     })
+  //     return;
+  //   }
 
-    param = {
-      goods_id: goods.goods_id,
-      model_id: goods.model_id || '',
-      num: targetNum,
-      sub_shop_app_id: that.franchisee_id,
-      is_seckill : goods.is_seckill == 1 ? 1 : ''
-    };
-    app.sendRequest({
-      hideLoading: true,
-      url: '/index.php?r=AppShop/addCart',
-      data: param,
-      success: function(res){
-        that.cart_data_arr[index].num = targetNum;
-        data['goodsList['+index+'].num'] = targetNum;
-        data.selectDiscountInfo = '';
-        that.setData(data);
-        that.getCalculationInfo();
-      },
-      fail: function(res){
-        data = {};
-        that.cart_data_arr[index].num = currentNum;
-        data['goodsList['+index+'].num'] = currentNum;
-        that.setData(data);
-      }
-    })
-  },
-  deleteGoods: function(index){
-    var goodsList = this.data.goodsList,
-        that = this,
-        listExcludeDelete;
+  //   param = {
+  //     goods_id: goods.goods_id,
+  //     model_id: goods.model_id || '',
+  //     num: targetNum,
+  //     sub_shop_app_id: that.franchisee_id,
+  //     is_seckill : goods.is_seckill == 1 ? 1 : ''
+  //   };
+  //   app.sendRequest({
+  //     hideLoading: true,
+  //     url: '/index.php?r=AppShop/addCart',
+  //     data: param,
+  //     success: function(res){
+  //       that.cart_data_arr[index].num = targetNum;
+  //       data['goodsList['+index+'].num'] = targetNum;
+  //       data.selectDiscountInfo = '';
+  //       that.setData(data);
+  //       that.getCalculationInfo();
+  //     },
+  //     fail: function(res){
+  //       data = {};
+  //       that.cart_data_arr[index].num = currentNum;
+  //       data['goodsList['+index+'].num'] = currentNum;
+  //       that.setData(data);
+  //     }
+  //   })
+  // },
+  // deleteGoods: function(index){
+  //   var goodsList = this.data.goodsList,
+  //       that = this,
+  //       listExcludeDelete;
 
-    app.sendRequest({
-      url : '/index.php?r=AppShop/deleteCart',
-      method: 'post',
-      data: {
-        cart_id_arr: [this.cart_data_arr[index].cart_id],
-        sub_shop_app_id: this.franchisee_id
-      },
-      success: function(res){
-        (listExcludeDelete = goodsList.concat([])).splice(index, 1);
-        if(listExcludeDelete.length == 0){
-          app.turnBack();
-          return;
-        }
+  //   app.sendRequest({
+  //     url : '/index.php?r=AppShop/deleteCart',
+  //     method: 'post',
+  //     data: {
+  //       cart_id_arr: [this.cart_data_arr[index].cart_id],
+  //       sub_shop_app_id: this.franchisee_id
+  //     },
+  //     success: function(res){
+  //       (listExcludeDelete = goodsList.concat([])).splice(index, 1);
+  //       if(listExcludeDelete.length == 0){
+  //         app.turnBack();
+  //         return;
+  //       }
 
-        var deleteGoodsId = goodsList[index],
-            noSameGoodsId = true;
+  //       var deleteGoodsId = goodsList[index],
+  //           noSameGoodsId = true;
 
-        for (var i = listExcludeDelete.length - 1; i >= 0; i--) {
-          if(listExcludeDelete[i].id == deleteGoodsId){
-            noSameGoodsId = false;
-            break;
-          }
-        }
-        if(noSameGoodsId){
-          delete that.additional_info[deleteGoodsId];
-        }
-        that.cart_data_arr.splice(index, 1);
-        that.setData({
-          goodsList: listExcludeDelete,
-          selectDiscountInfo: '',
-          exchangeCouponData: {
-            dialogHidden: true,
-            hasSelectGoods: false,
-            voucher_coupon_goods_info: {}
-          }
-        })
-        that.getCalculationInfo();
-      }
-    });
-  },
+  //       for (var i = listExcludeDelete.length - 1; i >= 0; i--) {
+  //         if(listExcludeDelete[i].id == deleteGoodsId){
+  //           noSameGoodsId = false;
+  //           break;
+  //         }
+  //       }
+  //       if(noSameGoodsId){
+  //         delete that.additional_info[deleteGoodsId];
+  //       }
+  //       that.cart_data_arr.splice(index, 1);
+  //       that.setData({
+  //         goodsList: listExcludeDelete,
+  //         selectDiscountInfo: '',
+  //         exchangeCouponData: {
+  //           dialogHidden: true,
+  //           hasSelectGoods: false,
+  //           voucher_coupon_goods_info: {}
+  //         }
+  //       })
+  //       that.getCalculationInfo();
+  //     }
+  //   });
+  // },
   // 去付款
   confirmPayment: function(e){
     // 模拟支付结果
@@ -411,7 +416,6 @@ Page({
       },
       success: function (res) {
         var param = res.data;
-
         param.orderId = orderId;
         param.success = paySuccess;
         param.goodsType = 0;
@@ -449,221 +453,26 @@ Page({
       }
     })
   },
-  discountChange: function(e){
-    var index = e.detail.value;
-
-    this.setData({
-      selectDiscountInfo: this.data.discountList[index],
-      selectDiscountIndex: index,
-      'exchangeCouponData.hasSelectGoods': false,
-      'exchangeCouponData.voucher_coupon_goods_info': {}
-    })
-    this.getCalculationInfo();
-  },
-  goToMyAddress: function () {
-    var addressId = this.data.selectAddress.id;
-    this.isFromSelectAddress = true;
-    app.turnToPage('/pages/myAddress/myAddress?id=' + addressId);
-  },
-  showAddAddress: function () {
-    var _this = this;
-
-    app.chooseAddress({
-      success: function (res) {
-        app.sendRequest({
-          method: 'post',
-          url: '/index.php?r=AppShop/AddWxAddress',
-          data: {
-            detailInfo: res.detailInfo || '',
-            cityName: res.cityName || '',
-            provinceName: res.provinceName || '',
-            UserName: res.userName || '',
-            telNumber: res.telNumber || '',
-            district: res.district || '',
-            countyName: res.countyName || ''
-          },
-          success: function () {
-            _this.dataInitial();
-          },
-        })
-      }
-    })
-  },
-  makeStorePhoneCall: function(){
-    app.makePhoneCall(this.data.shopAddress.shop_contact);
-  },
-  openStoreLocation: function(){
-    var infor = this.data.shopAddress.region_string + this.data.shopAddress.shop_location;
-
-    infor = infor.replace(/\s/g,'');
-    app.sendRequest({
-      url: '/index.php?r=Map/GetLatAndLngByAreaInfo',
-      data: {
-        location_info: infor
-      },
-      success: function (res) {
-        if (res.result){
-          wx.openLocation({
-            latitude: res.result.location.lat,
-            longitude: res.result.location.lng
-          })
-        }
-      }
-    });
-  },
   useBalanceChange: function(e){
+    // 重新计算价格
+    this.getCalculationInfo()
+    var haveIntegrals = this.data.haveIntegrals,
+      totalPrice = this.data.totalPayment,
+      useIntegrals = 0, // 使用的积分
+      deduction = 0;// 积分折扣金额
+    if(haveIntegrals <= totalPrice){
+      useIntegrals = haveIntegrals;
+      deduction = useIntegrals;
+      totalPrice = totalPrice - deduction
+    }else{
+      useIntegrals = Math.ceil(totalPrice);
+      deduction = totalPrice;
+      totalPrice = totalPrice - deduction
+    }
     this.setData({
-      useBalance: e.detail.value
-    });
-    this.getCalculationInfo();
-  },
-  deliveryWayChange: function(e){
-    this.setData({
-      is_self_delivery: e.detail.value
-    })
-    this.getCalculationInfo();
-  },
-  goToAdditionalInfo: function(){
-    app.setGoodsAdditionalInfo(this.additional_info);
-    app.turnToPage('/eCommerce/pages/goodsAdditionalInfo/goodsAdditionalInfo');
-  },
-  exchangeCouponInit: function(id){
-    var _this = this;
-    app.sendRequest({
-      url: '/index.php?r=AppShop/getGoods',
-      data: {
-        data_id: id
-      },
-      success: function (res) {
-        var goods = res.data[0].form_data;
-        var goodsModel = [];
-        var selectModelInfo = {
-          'models': [],
-          'price': 0,
-          'modelId': '',
-          'models_text': '',
-          'imgurl': ''
-        };
-        if(goods.model_items.length){
-          // 有规格
-          selectModelInfo['price'] = Number(goods.model_items[0].price);
-          selectModelInfo['imgurl'] = goods.model_items[0].img_url;
-          selectModelInfo['modelId'] = goods.model_items[0].id;
-        } else {
-          selectModelInfo['price'] = Number(goods.price);
-          selectModelInfo['imgurl'] = goods.cover;
-        }
-        for(var key in goods.model){
-          if(key){
-            goodsModel.push(goods.model[key]); // 转成数组
-            selectModelInfo['models'].push(goods.model[key].subModelId[0]);
-            selectModelInfo['models_text'] += '“' + goods.model[key].subModelName[0] + '” ';
-          }
-        }
-        goods.model = goodsModel; // 将原来的结构转换成数组
-        _this.setData({
-          'exchangeCouponData.dialogHidden': false, // 显示模态框
-          'exchangeCouponData.goodsInfo': goods,
-          'exchangeCouponData.selectModelInfo': selectModelInfo
-        });
-      }
+      useIntegrals: useIntegrals,
+      deduction: deduction,
+      totalPayment: totalPrice
     });
   },
-  exchangeCouponHideDialog: function(){
-    this.setData({
-      selectDiscountInfo: this.data.discountList[0],
-      selectDiscountIndex: 0,
-      'exchangeCouponData.dialogHidden': true,
-      'exchangeCouponData.hasSelectGoods': false,
-      'exchangeCouponData.voucher_coupon_goods_info': {}
-    })
-    this.getCalculationInfo();
-  },
-  exchangeCouponSelectSubModel: function(e){
-    var dataset = e.target.dataset,
-        modelIndex = dataset.modelIndex,
-        submodelIndex = dataset.submodelIndex,
-        data = {},
-        selectModels = this.data.exchangeCouponData.selectModelInfo.models,
-        model = this.data.exchangeCouponData.goodsInfo.model,
-        text = '';
-
-    selectModels[modelIndex] = model[modelIndex].subModelId[submodelIndex];
-
-    // 拼已选中规格文字
-    for (let i = 0; i < selectModels.length; i++) {
-      let selectSubModelId = model[i].subModelId;
-      for (let j = 0; j < selectSubModelId.length; j++) {
-        if( selectModels[i] == selectSubModelId[j] ){
-          text += '“' + model[i].subModelName[j] + '” ';
-        }
-      }
-    }
-    data['exchangeCouponData.selectModelInfo.models'] = selectModels;
-    data['exchangeCouponData.selectModelInfo.models_text'] = text;
-
-    this.setData(data);
-    this.exchangeCouponResetSelectCountPrice();
-  },
-  exchangeCouponResetSelectCountPrice: function(){
-    var _this = this,
-        selectModelIds = this.data.exchangeCouponData.selectModelInfo.models.join(','),
-        modelItems = this.data.exchangeCouponData.goodsInfo.model_items,
-        data = {};
-
-    for (var i = modelItems.length - 1; i >= 0; i--) {
-      if(modelItems[i].model == selectModelIds){
-        data['exchangeCouponData.selectModelInfo.stock'] = modelItems[i].stock;
-        data['exchangeCouponData.selectModelInfo.price'] = modelItems[i].price;
-        data['exchangeCouponData.selectModelInfo.modelId'] = modelItems[i].id;
-        data['exchangeCouponData.selectModelInfo.imgurl'] = modelItems[i].img_url;
-        break;
-      }
-    }
-    this.setData(data);
-  },
-  exchangeCouponConfirmGoods: function(){
-    let _this = this;
-    let goodsInfo = _this.data.exchangeCouponData.goodsInfo;
-    let model = goodsInfo.model;
-    let selectModels = _this.data.exchangeCouponData.selectModelInfo.models;
-    let model_value_str = '';
-    if(selectModels.length > 0){
-      model_value_str = '(';
-      for (let i = 0; i < selectModels.length; i++) {
-        let selectSubModelId = model[i].subModelId;
-        for (let j = 0; j < selectSubModelId.length; j++) {
-          if( selectModels[i] == selectSubModelId[j] ){
-            model_value_str += model[i].subModelName[j] + '|';
-          }
-        }
-      }
-      model_value_str += ')';
-    }
-    goodsInfo['model_value_str'] = model_value_str;
-    _this.setData({
-      'exchangeCouponData.dialogHidden': true,
-      'exchangeCouponData.selectModelInfo': {},
-      'exchangeCouponData.hasSelectGoods': true,
-      'exchangeCouponData.voucher_coupon_goods_info': {
-        goods_id: goodsInfo.id,
-        num: 1,
-        model_id: _this.data.exchangeCouponData.selectModelInfo.modelId
-      },
-      'exchangeCouponData.goodsInfo': goodsInfo
-    });
-    _this.getCalculationInfo();
-  },
-  inputGoodsCount: function (e) {
-    let value = +e.detail.value;
-    let index = e.target.dataset.index;
-
-    if (isNaN(value) || value <= 0) {
-      return;
-    }
-    clearTimeout(this.inputTimer);
-    this.inputTimer = setTimeout(() => {
-      this.changeGoodsNum(index, value);
-    }, 500);
-  }
 })
