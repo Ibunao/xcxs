052a88d17e23b65976f161d2e4e4136afcdf44c1
ibunao <idingran@163.com>
2018-10-30 23:59:29 +0800

    展示页

--- random/pages/myList/myList.js
+++ random/pages/myList/myList.js
@@ -5,9 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      imgSwitchIndex: 0,
+      items:[
+        {
+          qishu: '第2018126期',
+          riqi: '2018-10-28',
+          one: "01",
+          two: "02",
+          three: "03",
+          four: "04",
+          five: "05",
+          six: "06",
+          seven: "07",
+          open:false
+        },
+        {
+          qishu: '第2018126期',
+          riqi: '2018-10-28',
+          one: "01",
+          two: "02",
+          three: "03",
+          four: "04",
+          five: "05",
+          six: "06",
+          seven: "07",
+          open:false
+        }
+      ]
   },
-
+  /**
+   * 切换下拉
+   */
+  xiala: function (e) {
+    var switchIndex = e.target.dataset.index
+    this.data.items[switchIndex].open = !this.data.items[switchIndex].open;
+    this.setData({
+        imgSwitchIndex: switchIndex,
+        items: this.data.items
+      })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
