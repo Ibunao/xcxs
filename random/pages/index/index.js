f1d4c5f67d48153fa2a08b262c0275378708fce9
ibunao <idingran@163.com>
2018-11-02 00:02:16 +0800

    比一比

--- random/pages/index/index.js
+++ random/pages/index/index.js
@@ -4,51 +4,83 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    tabIndex: 1,
+    shareList:[
+      {
+        date: "2018-12-32",
+        qishu: "20180930",
+        jieguo: "未开奖",
+        items:[
+          {
+            img:'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoHZkDvnuY17Rksiatk1AX5htF7wZYAibcPyib1cibd7wLO2GYthwTsADIibzjia19Ou77oddS40UvLukYg/132',
+            name:"1**0",
+            balls:[
+              "01",
+              "01",
+              "01",
+              "01",
+              "01",
+              "01",
+              "01",
+            ],
+            result: "中10000元",
+            open:false
+          },
+          {
+            img: '',
+            name: "100-20",
+            balls: [
+              "01",
+              "01",
+              "01",
+              "01",
+              "01",
+              "01",
+              "01",
+            ],
+            result: "中10000元",
+            open:false,
+          }
+        ]
+      }, 
+      {
+        date: "2018-12-32",
+        qishu: "20180930",
+        jieguo: "未开奖",
+        items: [
+          {
+            img: '',
+            name: "100-20",
+            balls: [
+              "01",
+              "01",
+              "01",
+              "01",
+              "01",
+              "01",
+              "01",
+            ],
+            result: "中10000元",
+            open:false
+          }
+        ]
+      }
+    ]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  switchTab:function(e){
+    var index = e.target.dataset.index
+    this.setData({tabIndex: index})
+  },
+  /**
+   * 切换下拉
+   */
+  xiala: function (e) {
+    var switchIndex = e.target.dataset.index
+    this.data.shareList[switchIndex].open = !this.data.shareList[switchIndex].open;
+    this.setData({
+      shareList: this.data.shareList
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
